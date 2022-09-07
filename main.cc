39a50b4cd795e21cb6982f811f76b030a89b5c25
SaichetanKamisetty <90051818+SaichetanKamisetty@users.noreply.github.com>
2022-09-07 11:58:47 -0700

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,29 +6,85 @@
 
 
 int main() {
-  vector131<int> car{3, 4, 5, 6};
-  car.push_back(3);
-  car.push_back(5);
-  car.push_back(10);
-  car.push_back(4);
+    vector131<int> v{3, 4, 5, 6};
+    vector131<std::string> v2;
 
-  car.push_front(31);
-  car.push_front(34);
-  car.push_front(12);
-  car.push_front(331);
+    // Push_Back Method
 
-  car.pop_back();
-  car.pop_back();
-  car.pop_back();
-  car.pop_back();
+    v.push_back(3);
+    v.push_back(5);
+    v.push_back(10);
+    v.push_back(4);
 
-  car.resize(3);
-  car.resize(10);
+    v2.push_back("aaa");
+    v2.push_back("bbb");
+    v2.push_back("ccc");
 
-  for (int i = 0; i < car.size(); i++) {
-    std::cout << car[i] << " ";
-  } 
-  std::cout << "\n";
+    v.elements();
+    v2.elements();
 
-  return 0;
+    std::cout << std::endl;
+
+    // Push_Front Method
+
+    v.push_front(31);
+    v.push_front(34);
+    v.push_front(12);
+
+    v2.push_front("ddd");
+    v2.push_front("eee");
+    v2.push_front("fff");
+
+    v.elements();
+    v2.elements();
+
+    std::cout << std::endl;
+
+    // Pop_Back Method
+
+    v.pop_back();
+    v.pop_back();
+    v.pop_back();
+
+    v2.pop_back();
+    v2.pop_back();
+    v2.pop_back();
+
+    v.elements();
+    v2.elements();
+
+    std::cout << std::endl;
+
+    // Resize Method
+
+    v.resize(3);
+    v.resize(15);
+
+    v2.resize(1);
+    v2.resize(12);
+
+    v.elements();
+    v2.elements();
+
+    std::cout << std::endl;
+
+    // Insert Method
+
+    v.insert(13, 420);
+    v2.insert(10, "mark");
+
+    v.elements();
+    v2.elements();
+
+    std::cout << std::endl;
+
+    // Clear Method
+
+    v.clear();
+    v2.clear();
+
+    v.elements();
+    v2.elements();
+
+    return 0;
 }
